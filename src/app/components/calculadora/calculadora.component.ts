a00e7d4c8fec87f674f42c35abc01e6d8a2065a2
Victor <medina.levic24@triana.salesianos.edu>
2024-10-15 12:26:39 +0200

    casi casi

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -6,19 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent implements OnInit {
-  
 
-  operacion = '';
 
-  addNum(arg0: number) {
-    this.operacion += arg0;
+
+
+
+
+  ecuacion = '';
+  ecuacion2 = '';
+
+  addNum(arg0: string) {
+    this.ecuacion += arg0;
   }
-  
 
+  operacion(simbolo: string) {
+    if(simbolo === '+') {
+      
+     
+    }
+
+  }
 
 
   clean() {
-    this.operacion = '';
+    this.ecuacion = '';
+    this.ecuacion2 = '';
   }
   ngOnInit(): void {
   }
